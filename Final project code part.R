38ab4710e0a9041ca7d338377f73350bae893a86
Yu Gu <yzg0034@auburn.edu>
2020-11-16 16:59:06 -0500

    fix the code error

--- Final project code part.R
+++ Final project code part.R
@@ -1,3 +1,13 @@
+#Here is the example to test the code:
+getwd()
+forestfires<- read.csv("forestfires.csv")
+y<-forestfires$Y
+x<-forestfires[,c("X","DC")]
+alpha<-0.05
+
+my_lm(yy,xx,0.05)
+
+
 # The first part of the code is from section 6.4 of the book
 # linear regression model
 my_lm = function(y, x, alpha) {
@@ -18,14 +28,17 @@ beta.hat <- solve(t(x)%*%x)%*%t(x)%*%y
 # Compute residuals(n*1)
 resid <- y - x%*%as.matrix(beta.hat) 
 sigma2.hat <- (1/df)*t(resid)%*%resid
-  
+sigma2.hat<-as.numeric(sigma2.hat)
 # Estimate of the variance of the estimated beta from Eq. (6.2)
 var.beta <- sigma2.hat*solve(t(x)%*%x)
-##########################################################  
+var.beta<-diag(var.beta)
+###################################################### 
 ## Estimate of the confidence interval based on alpha
   quant <- 1 - alpha/2
-  ci.beta <- c(beta.hat - qnorm(p = quant)*sqrt(var.beta), beta.hat + 
+  ci.beta <- cbind(beta.hat - qnorm(p = quant)*sqrt(var.beta), beta.hat + 
                  qnorm(p = quant)*sqrt(var.beta))
+  df <- as.data.frame(ci.beta)
+  colnames(df) <- c("low", "high")
   
 ######################################################
 #plot
@@ -51,9 +64,11 @@ fig3<-plot(resid,densi,
 main="Histogram (or density) of residuals",
 xlab="residual",
 ylab="density")
+
 ######################################################
 #Mean Square Prediction Error (MSPE)
 MSPE<- (1/n)*t(resid)%*%resid
+
 ######################################################
 #F-test
 yaverge<-mean(y)
@@ -68,6 +83,6 @@ Probofftest<-pf(fstar, p-1, df, lower.tail = FALSE)
 # Return all estimated values
 return(list( ci = ci.beta,fig1,fig2,fig3,MSPE=MSPE,
              Ftest=Fstar,Probability=Probofftest))
-}
 
+}
 
